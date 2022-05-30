6a9168fa569998aec2d8f49874400621a2c0450d
晨风 <cezres@163.com>
2022-05-30 10:38:21 +0800

    Initial project

---
+++ CloudAssets/CloudAssets.h
@@ -0,0 +1,18 @@
+//
+//  CloudAssets.h
+//  CloudAssets
+//
+//  Created by azusa on 2022/5/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CloudAssets.
+FOUNDATION_EXPORT double CloudAssetsVersionNumber;
+
+//! Project version string for CloudAssets.
+FOUNDATION_EXPORT const unsigned char CloudAssetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudAssets/PublicHeader.h>
+
+
